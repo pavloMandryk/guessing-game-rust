75dc97fa6e50a97aa6388b98bf802edd58fb0959
pavloMandryk <mandryk96@gmail.com>
2023-07-15 20:50:43 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("This is the secret number: {secret_number}");
+
+    println!("Guess a number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess
+        .trim()
+        .parse()
+        .expect("Please type a number!");
+
+    println!("Your guess: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+}
